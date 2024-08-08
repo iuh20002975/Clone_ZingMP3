d4fc4fe4f136e5b20ee9516a41ddb08b542fbe0c
Nguyen Long <longsky0912624119@gmail.com>
2024-08-08 10:46:07 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <div className="">
       app
     </div>
+    <ToastContainer
+    position="bottom-right"
+    autoClose={5000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    theme="light"
+    />
+    </>
   );
 }
 
